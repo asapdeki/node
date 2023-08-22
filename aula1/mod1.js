c8ced9d9d790f940a2399093ec3ddc3c4340cc08
gutakeuchi <gustavo.takeuchi87@gmail.com>
2023-08-22 16:42:17 -0300

    first commit

---
+++ aula1/mod1.js
@@ -0,0 +1,24 @@
+// const nome = 'Gustavo';
+// const sobrenome = 'Takeuchi';
+
+// const falaNome = () => nome + ' ' + sobrenome
+
+// // module.exports.nome = nome;
+// // module.exports.sobrenome = sobrenome;
+// // module.exports.falaNome = falaNome;
+
+// exports.nome = nome;
+// exports.sobrenome = sobrenome;
+// exports.falaNome = falaNome;
+// this.qualquerCoisa = 'O que eu quiser exportar';
+
+// console.log(module.exports);
+
+class Pessoa {
+    constructor(nome){
+        this.nome = nome;
+    }
+}
+
+exports.Pessoa = Pessoa;
+
---
+++ aula2/app.js
@@ -0,0 +1,9 @@
+const Cachorro = require('./Z/mod2.js');
+
+const c1 = new Cachorro('Dog');
+
+c1.latir();
+
+// const path = require('path');
+// console.log(path.resolve(__dirname));
+// console.log(path.resolve(__dirname, '..', 'arquivos', 'imagens'));
